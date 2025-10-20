f5682402c4acec3a243015fa389e3c8172fb22e1
Darus Salam <darussalam66d@gmail.com>
2025-10-20 09:32:15 +0600

    on_unknown_route

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
         "/home":(context)=>HomeScreen(),
         '/second':(context)=>SecondScreen(),
       },
+
       onUnknownRoute: (settings){
         return MaterialPageRoute(
             builder:(_)=> Scaffold(
